7454c57a3918603bfb48fb2f7cc8eaaba5c2b3e2
Maria S <maria.stan1709@gmail.com>
2019-10-25 12:00:51 +0200

    added POST API for user, updated server.js

---
+++ routes/users.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const route = express.Router();
+
+// endpoint GET---------------------------------
+route.get('/:userID', function(req, res){
+    
+    let user = db.getUser(req.body.userID);
+    // function name/ structure to change based on the db module
+
+    if(user){
+        res.status(200).json(user);
+    }
+    else{
+        res.status(404).end();
+        //more complex error handling to come
+    }
+
+});
+
+// endpoint POST--------------------------------
+route.post('/', function(req,res){
+
+    let username = req.body.username;
+    let password = req.body.password;
+    let email = req.body.email;
+    //to be tested after creating the user
+
+});
+
+
+module.export = route;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const DEFAULT_PORT = 8080;
+app.set('port', (process.env.PORT || DEFAULT_PORT));
+
+const userRoutes = require('./routes/users');
+app.use('/user', userRoutes);
+
+
+// start server------------------------------
+app.listen(app.get('port'), function(){
+    console.log("server running ", app.get('port'));
+});
