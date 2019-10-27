b3c70bfc093567fa098a8859d7e4b59d79e698b3
toralfs <toralfs97@gmail.com>
2019-10-27 14:27:06 +0100

    query fails, try to remove params from runQuery and WHERE from query

--- modules/db.js
+++ modules/db.js
@@ -3,10 +3,10 @@ const pg = require('pg');
 const db = function(dbConnectionString) {
     const connectionString = dbConnectionString;
 
-    async function runQuery(query, params) {
+    async function runQuery(query) {
         const client = new pg.Client(connectionString);
         await client.connect();
-        const res = await client.query(query, params);
+        const res = await client.query(query);
         let response = res.rows[0];
         await client.end();
         return response;
@@ -16,7 +16,7 @@ const db = function(dbConnectionString) {
         console.log(userID);
         let userData = null;
         try {
-            userData = await runQuery(`SELECT * FROM users WHERE userID=$1`, [userID]);
+            userData = await runQuery(`SELECT * FROM users`);
             console.log(userData);
         } catch(error) {
             console.error("someting wrong");
