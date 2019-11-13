a863c290ff06ee2d91ef8c81dcced4102a0f2857
toralfs <toralfs97@gmail.com>
2019-11-13 19:48:41 +0100

    added public column (bool) to presentation to decide if presentation is public or not.

--- modules/db.js
+++ modules/db.js
@@ -129,7 +129,7 @@ const db = function (dbConnectionString) {
     const insertPresentation = async function (presentationName, ownerID, theme) {
         let response = null;
         try {
-            await runQuery(`INSERT INTO presentations(name, slides, ownerID, sharedUsers, theme) VALUES ($1, '{}', $2, '{}', $3)`, [presentationName, ownerID, theme]);
+            await runQuery(`INSERT INTO presentations(name, slides, ownerID, sharedUsers, public, theme) VALUES ($1, '{}', $2, '{}', false, $3)`, [presentationName, ownerID, theme]);
             response = DB_RESPONSES.OK;
         } catch (error) {
             console.error(error);
