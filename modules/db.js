632e1ce9ff4f8b05204d7994a7bef8dfb51af803
toralfs <toralfs97@gmail.com>
2019-11-13 23:39:31 +0100

    top nav bar about finshed. Client gets presentation from server and shows names on page, no css on that part yet. Very barebones at the moment, no user support etc.

--- modules/db.js
+++ modules/db.js
@@ -182,7 +182,7 @@ const db = function (dbConnectionString) {
         try {
             let insertedSlide = await runQuery(`INSERT INTO slides (data, presentationID) VALUES ($1, $2) RETURNING slideID`, [data, presentationID]);
             let slideID = insertedSlide.slideid;
-            await runQuery(`UPDATE presentations SET slides = slides || $1::int WHERE presentationID = $2`, [slideID, presentationID]);
+            await runQuery(`UPDATE presentations SET last_updated=current_timestamp, slides = slides || $1::int WHERE presentationID = $2`, [slideID, presentationID]);
             response = DB_RESPONSES.OK; 
         } catch (error) {
             console.error(error);
