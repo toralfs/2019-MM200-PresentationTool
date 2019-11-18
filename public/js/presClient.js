e5db3c5cc9b051c94edd2a8c8bea34a9ff7537ad
toralfs <toralfs97@gmail.com>
2019-11-18 19:49:33 +0100

    Moved all javascript from presClient.html to presClient.js. Moved mobile nav functions to seperate script (mobile.js)

---
+++ public/js/presClient.js
@@ -0,0 +1,359 @@
+let presOverview = document.getElementById("overview");
+let editView = document.getElementById("editview");
+let pageList = [presOverview, editView];
+console.log(pageList);
+
+let divSelectedSlide = document.getElementById("selectedSlide");
+let presContainer = document.getElementById("presContainer");
+let slideList = document.getElementById("slide__list");
+let divList = [divSelectedSlide, presContainer, slideList];
+
+// --------------------------------------------------------------
+const HTTP_CODES = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    CONFLICT: 409
+}
+
+// ---------------- Presentations ---------------------------
+let userPresentations = [];
+
+let currentPres = {
+    ID: "",
+    owner: "",
+    theme: ""
+}
+
+let selectedSlide = {
+    ID: "",
+    data: {}
+}
+
+const restAPI = {
+    createPresentation: async function (name, ownerID, theme) {
+
+        let updata = {
+            name: name,
+            ownerID: ownerID,
+            theme: theme
+        }
+
+        let cfg = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updata)
+        }
+
+        try {
+            let resp = await fetch(`/presentation/`, cfg);
+            let data = await resp.json();
+            return data;
+        }
+        catch (err) {
+            console.log(err);
+        }
+
+    },
+
+    //----------------------------
+    deletePresentation: async function (presID) {
+
+        let cfg = {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json" }
+        }
+
+        try {
+            let resp = await fetch(`/presentation/${presID}`, cfg);
+            let data = await resp.json();
+            return data;
+
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+
+    //----------------------------
+    updatePresentation: async function (presID, name, theme) {
+
+        let updata = {
+            name: name,
+            theme: theme
+        }
+
+        let cfg = {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updata)
+        }
+
+        try {
+            let resp = await fetch(`/presentation/${presID}`, cfg);
+            let data = await resp.json();
+            return data;
+
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+
+    getPresentations: async function (ownerID) {
+        try {
+            let res = await fetch(`/presentation/${ownerID}`);
+            let data = await res.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
+    // ------------------ Slides ----------------------
+    getSlides: async function (presID) {
+        try {
+            let resp = await fetch(`/presentation/slide/${presID}`);
+            let data = await resp.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    updateSlide: async function (slideID, slideData) {
+
+        let updata = {
+            data: slideData
+        }
+
+        let cfg = {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updata)
+        }
+
+        try {
+            let resp = await fetch(`/presentation/slide/${slideID}`, cfg);
+            let data = await resp.json();
+            return data;
+
+        }
+        catch (err) {
+            console.log(err);
+        }
+    },
+
+    // ------------------ Sharing ---------------------
+    setPublicStatus: async function (presID, status) {
+        let updata = {
+            public: status
+        };
+
+        let cfg = {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updata)
+        }
+
+        try {
+            let resp = await fetch(`/presentation/${presID}/public`, cfg);
+            let data = resp.json();
+            return data;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    },
+
+    shareWithUser: async function (presID, username) {
+        let updata = {
+            username: username
+        };
+
+        let cfg = {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updata)
+        }
+
+        try {
+            let resp = await fetch(`/presentation/${presID}/share`, cfg);
+            let data = resp.json();
+            return data;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    },
+
+    unshareWithUser: async function (presID, userID) {
+        let updata = {
+            userID: userID
+        };
+
+        let cfg = {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(updata)
+        }
+
+        try {
+            let resp = await fetch(`/presentation/${presID}/unshare`, cfg);
+            let data = resp.json();
+            return data;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    },
+
+    getPublicPresentations: async function () {
+        try {
+            let resp = await fetch(`/presentation/public`);
+            let data = await resp.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    getSharedWithMePresentations: async function (userID) {
+        try {
+            let resp = await fetch(`/presentation/${userID}/shared-with-me`);
+            let data = await resp.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+
+async function loadPresOverview(isShared) {
+    userPresentations = [];
+
+    hideAllPages(pageList, divList);
+    presOverview.style.display = "block"; //display style subject to change
+
+    let presentations = null;
+    if (isShared) {
+        presentations = await restAPI.getSharedWithMePresentations(3);
+        presOverview.querySelector("h1").innerHTML = "Shared with me"
+    } else {
+        presentations = await restAPI.getPresentations(1);
+        presOverview.querySelector("h1").innerHTML = "My Presentations"
+    }
+    //need to transfer the ID of the current user (from local storage)
+    if (presentations.code === HTTP_CODES.OK) {
+        for (let presentation of presentations.presentations) {
+            userPresentations.push(presentation);
+            let tmp1 = document.getElementById('overviewTemp').content.cloneNode(true);
+
+            tmp1.querySelector("div").addEventListener("click", function (e) {
+                initEditPresentation(e);
+            });
+            tmp1.querySelector(".overview-presname").innerText = presentation.name;
+            tmp1.querySelector(".overview-slides").innerText += presentation.slides.length;
+            tmp1.querySelector(".overview-theme").innerText += presentation.theme;
+            tmp1.querySelector(".overview-hiddenid").innerText = presentation.presentationid;
+            if (presentation.public == true) {
+                tmp1.querySelector(".overview-public-status").innerText += "Public";
+            }
+            else {
+                tmp1.querySelector(".overview-public-status").innerText += "Private";
+            }
+
+            let lastUpdated = splitTime(presentation.last_updated);
+
+            tmp1.querySelector(".overview-last-updated").innerText += `${lastUpdated.date}, ${lastUpdated.clock}`;
+
+            presContainer.appendChild(tmp1);
+        }
+    } else {
+        presContainer.innerHTML = `Error ${presentations.code}, ${presentations.msg}`; //Should be a describing error message
+    }
+
+}
+
+async function createPresentation() {
+    let createdPres = await restAPI.createPresentation("New presentation", currentUser.ID, "default");
+    console.log(createdPres);
+    if (createdPres.code === HTTP_CODES.CREATED) {
+        //Load edit view with created presentation
+    }
+}
+
+function displaySlide(slide, parentDiv) {
+    let tmp1 = document.getElementById(`temp-slide${slide.data.type}`).content.cloneNode(true);
+
+    tmp1.querySelector(".title").innerText = slide.data.title;
+    parentDiv.appendChild(tmp1);
+}
+
+function initEditPresentation(e) {
+    let presID = parseInt(e.currentTarget.querySelector("div").innerText);
+
+    let index = userPresentations.map(function (e) {
+        return e.presentationid;
+    }).indexOf(presID);
+
+    currentPres.ID = userPresentations[index].presentationid;
+    currentPres.owner = userPresentations[index].ownerid;
+    currentPres.theme = userPresentations[index].theme;
+    hideAllPages(pageList, divList); //This seems like a bad idea
+    loadEditView(currentPres.ID);
+}
+
+async function loadEditView() {
+    window.location.href = "#editview";
+    hideAllPages(pageList, divList);
+    editView.style.display = "block";
+    divSelectedSlide.style.display = "block";
+
+    let slides = await restAPI.getSlides(currentPres.ID);
+    if (slides.code === HTTP_CODES.OK) {
+        if (slides.data.length > 0) {
+            for (let slide of slides.data) {
+                let tmp1 = document.getElementById("edit-slideoverview-temp").content.cloneNode(true);
+                tmp1.querySelector(".edit-slideoverview").innerText = `# ${slide.slideid}`;
+                slideList.appendChild(tmp1);
+            }
+            selectedSlide.ID = slides.data[0].slideid;
+            selectedSlide.data = slides.data[0].data;
+
+            displaySlide(selectedSlide, divSelectedSlide);
+        } else {
+            divSelectedSlide.innerHTML = "You have no slides yet";
+        }
+    }
+}
+
+
+function splitTime (timestamp) {
+    let splitTimestamp = timestamp.split(/[T,.,]+/);
+    let time = {
+        date: splitTimestamp[0],
+        clock: splitTimestamp[1]
+    }
+    return time;
+}
+
+function changeTheme(presentation, selectedTheme) {
+    presentation.theme = selectedTheme;
+}
+
+function changeBgColor(slide, selectedColor) {
+    slide.data.bgColor = selectedColor;
+}
+
+function hideAllPages(pages, divs) {
+    for (let page of pages) {
+        page.style.display = "none";
+    }
+    for (let div of divs) {
+        while (div.firstChild) {
+            div.removeChild(div.firstChild);
+        }
+    }
+}
