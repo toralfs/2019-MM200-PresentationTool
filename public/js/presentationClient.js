dc6d8ae5aa74eca66283fb9f871edaf314dc9ba4
Maria S <maria.stan1709@gmail.com>
2019-11-18 21:08:11 +0100

    added code from presClient.html to index.html; added some css

--- public/js/presentationClient.js
+++ public/js/presentationClient.js
@@ -1,7 +1,7 @@
 function PresentationClient(){
 
     //----------------------------
-    this.createPresentation = async function(result, url, name, ownerID, theme){
+    this.createPresentation = async function(url, name, ownerID, theme){
 
         let updata = {
             name: name,
@@ -18,7 +18,7 @@ function PresentationClient(){
         try {
             let resp = await fetch(url, cfg);
             let data = await resp.json();
-            result.innerHTML = data.msg;
+            return data;
             
         }
         catch (err) {
@@ -79,6 +79,103 @@ function PresentationClient(){
         } catch (error) {
             console.error(error);
         }
+    }
+
+        // ------------------ Slides ----------------------
+        this.getSlides = async function(url, presID) {
+            try {
+                let resp = await fetch(url + presID);
+                let data = await resp.json();
+                return data;
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    
+    
+    
+        // ------------------ Sharing ---------------------
+        this.setPublicStatus = async function(url, status){
+            let updata = {
+                public: status
+            };
+            let cfg = {
+                method: "PUT",
+                headers: {"Content-Type":"application/json"},
+                body: JSON.stringify(updata)
+            }
+            try{
+                let resp = await fetch(url, cfg);
+                let data = resp.json();
+                return data;
+            }
+            catch(error){
+                console.log(error);
+            }
+        }
+        this.shareWithUser = async function(url, username){
+            let updata = {
+                username: username
+            };
+            let cfg = {
+                method: "PUT",
+                headers: {"Content-Type":"application/json"},
+                body: JSON.stringify(updata)
+            }
+            try{
+                let resp = await fetch(url, cfg);
+                let data = resp.json();
+                return data;
+            }
+            catch(error){
+                console.log(error);
+            }
+        }
+            
+    this.unshareWithUser = async function(url, userID){
+        let updata = {
+            userID: userID
+        };
+        let cfg = {
+            method: "PUT",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify(updata)
+        }
+        try{
+            let resp = await fetch(url, cfg);
+            let data = resp.json();
+            return data;
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+    
+    this.getPublicPresentations = async function (url) {
+        try {
+            let resp = await fetch(url);
+            let data = await resp.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    this.getSharedWithMePresentations = async function (url) {
+        try {
+            let resp = await fetch(url);
+            let data = await resp.json();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    this.splitTime = function(timestamp) {
+        let splitTimestamp = timestamp.split(/[T,.,]+/);
+        let time = {
+            date: splitTimestamp[0],
+            clock: splitTimestamp[1]
+        }
+        return time;
     }
     
 }
