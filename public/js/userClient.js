1b102c6953af429c20abf25314c8a2ba0420628f
Maria S <maria.stan1709@gmail.com>
2019-11-19 11:47:38 +0100

    moved script for user variables and functions to userClient.js

--- public/js/userClient.js
+++ public/js/userClient.js
@@ -1,7 +1,24 @@
-function UserClient(){
+let inpNameCreate = document.getElementById('inpNameCreate');
+let inpEmailCreate = document.getElementById('inpEmailCreate');
+let inpPasswordCreate = document.getElementById('inpPasswordCreate');
+let txtResultCreate = document.getElementById('txtResultCreate');
+
+let inpNameLogin = document.getElementById('inpNameLogin');
+let inpPasswordLogin = document.getElementById('inpPasswordLogin');
+let txtResultLogin = document.getElementById('txtResultLogin');
+
+let inpNameUpdate = document.getElementById('inpNameUpdate');
+let inpEmailUpdate = document.getElementById('inpEmailUpdate');
+let inpPasswordUpdate = document.getElementById('inpPasswordUpdate');
+let txtResultUpdate = document.getElementById('txtResultUpdate');
+
+let currentUser = {};
+
+// --------------------------------------------------------------
+const restAPIUser = {
 
     //------------------------
-    this.createUser = async function(result, url, username, email, password){
+    createUser: async function(result, url, username, email, password){
 
         let updata = {
             name: username,
@@ -24,10 +41,10 @@ function UserClient(){
         catch (err) {
             console.log(err);
         }
-    }
+    },
 
     //------------------------
-    this.loginUser = async function(result, url, username, password){
+    loginUser: async function(result, url, username, password){
 
         let updata = {
             name: username,
@@ -50,10 +67,10 @@ function UserClient(){
         catch(err){
             console.log(err);
         }
-    }
+    },
 
     //------------------------
-    this.updateUser = async function(result, url, username, email, password){ 
+    updateUser: async function(result, url, username, email, password){ 
         
         let updata = {
             name: username,
@@ -77,10 +94,10 @@ function UserClient(){
         catch(err){
             console.log(err);
         }
-    }
+    },
 
     //------------------------
-    this.deleteUser = async function(url){
+    deleteUser: async function(url){
         let cfg = {
             method: "DELETE",
             headers: {"Content-Type":"application/json"},
@@ -96,5 +113,108 @@ function UserClient(){
             console.log(err);
         }
     }
+}
 
+//Automatically login if the user did not logout from the previous session
+retrieveCredentials();
+
+//Store user credentials in Local Storage-------------------------------
+function saveCredentials() {
+    localStorage.setItem("userCredentials", JSON.stringify(currentUser));
+}
+
+function retrieveCredentials() {
+    let data = localStorage.getItem("userCredentials");
+    if (data) {
+        currentUser = JSON.parse(data);
+        showPresentationPage();
+    }
+    else {
+        currentUser.ID = "";
+        currentUser.username = "";
+        currentUser.email = "";
+    }
+}
+
+//------------------------------------------
+async function createUser() {
+    if (inputValidation.validName(txtResultCreate, inpNameCreate) && inputValidation.validEmail(txtResultCreate, inpEmailCreate)
+        && inputValidation.validPassword(txtResultCreate, inpPasswordCreate)) {
+        let currentData = await restAPIUser.createUser(txtResultCreate, "/user", inpNameCreate.value, inpEmailCreate.value, inpPasswordCreate.value);
+        if (currentData.code == HTTP_CODES.CREATED) {
+            currentUser.ID = currentData.userID;
+            currentUser.username = currentData.userName;
+            currentUser.email = currentData.userEmail;
+            inpNameUpdate.value = currentUser.username;
+            inpEmailUpdate.value = currentUser.email;
+            saveCredentials();
+            showPresentationPage();
+        }
+    }
+}
+
+//----------------------------------------
+async function loginUser() {
+    let currentData = await restAPIUser.loginUser(txtResultLogin, "/user/auth", inpNameLogin.value, inpPasswordLogin.value);
+    if (currentData.code == HTTP_CODES.OK) {
+        currentUser.ID = currentData.userID;
+        currentUser.username = currentData.userName;
+        currentUser.email = currentData.userEmail;
+        inpNameUpdate.value = currentUser.username;
+        inpEmailUpdate.value = currentUser.email;
+        saveCredentials();
+        showPresentationPage();
+    }
+}
+
+//---------------------------------------
+async function updateUser() {
+    if (inputValidation.validName(txtResultUpdate, inpNameUpdate) && inputValidation.validEmail(txtResultUpdate, inpEmailUpdate)
+        && inputValidation.validPassword(txtResultUpdate, inpPasswordUpdate)) {
+        let currentData = await restAPIUser.updateUser(txtResultUpdate, `/user/${currentUser.ID}`, inpNameUpdate.value, inpEmailUpdate.value, inpPasswordUpdate.value);
+        if (currentData.code == HTTP_CODES.OK) {
+            currentUser.username = currentData.userName;
+            currentUser.email = currentData.userEmail;
+            inpNameUpdate.value = currentUser.username;
+            inpEmailUpdate.value = currentUser.email;
+            saveCredentials();
+        }
+        inpPasswordUpdate.value = "";
+    }
+}
+
+//------------------------------------------
+function deleteUser() {
+    if (window.confirm("Are you sure you want to delete this account?")) {
+        let del = restAPIUser.deleteUser(`/user/${currentUser.ID}`);
+        if (del.code == HTTP_CODES.OK) {
+            currentUser = {};
+            emptyInputs();
+            showStartPage();
+            localStorage.clear();
+        }
+    }
+}
+
+//-----------------------------------------
+function logoutUser() {
+    currentUser = {};
+    emptyInputs();
+    showStartPage();
+    localStorage.clear();
+}
+
+//-----------------------
+function emptyInputs() {
+    let inputs = document.querySelectorAll("input");
+    for (let i of inputs) {
+        i.value = "";
+    }
+}
+
+//----------------------
+function emptyTxtResult() {
+    txtResultCreate.innerHTML = "";
+    txtResultLogin.innerHTML = "";
+    txtResultUpdate.innerHTML = "";
 }
